0041029ff79d36275f10d0eceb57895580c6c48c
To Tran <chimface2011@gmail.com>
2023-06-12 15:32:44 +0700

    update color

--- lua/abeltheme/groups/syntax.lua
+++ lua/abeltheme/groups/syntax.lua
@@ -17,8 +17,8 @@ local function callback()
     String = { fg = C.green, bg = C.none }, -- a string constant: "this is a string"
     Character = { fg = C.green, bg = C.none }, -- a character constant: 'c', '\n'
     Number = { fg = C.orange, bg = C.none }, -- a number constant: 234, 0xff
-    Boolean = { fg = C.blue, bg = C.none }, -- a boolean constant: TRUE, false
-    Float = { fg = C.yellow, bg = C.none }, -- a floating point constant: 2.3e10
+    Boolean = { fg = C.yellow, bg = C.none }, -- a boolean constant: TRUE, false
+    Float = { fg = C.orange, bg = C.none }, -- a floating point constant: 2.3e10
     -- Identifier
     Identifier = { fg = C.text, bg = C.none }, -- any variable name
     Function = { fg = C.blue, bg = C.none }, -- function name (also: methods for classes)
